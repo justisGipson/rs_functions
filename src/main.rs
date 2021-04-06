fdccadff7d43b3b9bcef93e234f2024747418d5a
Justis <jgipson@codelicious.com>
2021-04-06 15:02:03 -0400

    function examples

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+// basic function declaration
+//
+// fn main() {
+//     println!("Hello, world!");
+//     another_function();
+// }
+//
+// fn another_function() {
+//     println!("Another function");
+// }
+
+// function with parameters
+// fn main() {
+//     another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//     println!("The value of x is: {}", x);
+// }
+
+// function with multiple params
 fn main() {
-    println!("Hello, world!");
-    another_function();
+    another_function(5, 6);
 }
 
-fn another_function() {
-    println!("Another function");
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
